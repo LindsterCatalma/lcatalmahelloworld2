a12153a9d3e5451748475a5e924cd1acadcaad71
Lindster Carlo Catalma <Lindster.Carlo.Catalma@aa.com>
2021-06-05 02:09:54 +0800

    test

---
+++ src/main/java/com/testing/lcatalmahelloworld/LcatalmahelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.testing.lcatalmahelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcatalmahelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcatalmahelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/lcatalmahelloworld/LcatalmahelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.lcatalmahelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcatalmahelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
